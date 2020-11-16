fa90737bb5e4676cda2b46dc247ac9b1ed812703
artTanya <tanya_strelka@mail.ru>
2020-11-16 18:14:22 +0300

    change popup position

--- js/lang-selection.js
+++ js/lang-selection.js
@@ -1,12 +1,12 @@
 const langList = document.querySelector(".switch-lang__list");
 const langBtn = document.querySelector(".switch-lang__button");
 const switchLang = document.querySelector(".switch-lang");
-const inputs = switchLang.querySelectorAll('input');
+const inputs = switchLang.querySelectorAll("input");
 
 let addListener = function (element) {
-  element.addEventListener('change', function () {
+  element.addEventListener("change", function () {
     langBtn.innerHTML = element.value;
-    switchLang.classList.remove('switch-lang--open');
+    switchLang.classList.remove("switch-lang--open");
   });
 };
 
